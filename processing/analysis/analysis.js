ccc824c8585efb593618f4d2710e9ea57cc39509
Salma El Dessouky <salma.a.el.dessouky@eg.ibm.com>
2017-11-07 11:59:08 +0000

    last12

--- processing/analysis/analysis.js
+++ processing/analysis/analysis.js
@@ -252,9 +252,10 @@ function analyzeImage(args, fileName, analyzeCallback) {
             console.log('Image Keywords', err);
           } else if (body.images && body.images.length > 0) {
 
-        	 analysis.image_keywords = body.images[0].classifiers[1].classes;
-        	 analysis.image_keywords.concat(body.images[0].classifiers[0].classes);  
-        	 analysis.image_keywords.concat(body.images[0].classifiers[0].classes);  
+        	 analysis.image_keywords = body.images[0].classifiers[0].classes;
+        	 for (var i=0; i < body.images[0].classifiers[1].classes; i++) {
+    	    	 analysis.image_keywords.push(body.images[0].classifiers[1].classes[i]);  
+        	 }
           }
           callback(null);
         }));
